6c297a16f65fb4e64e1c7d5bab335a2d26ae0d02
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2022-03-05 16:59:10 +0100

    v0

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include_once "connessione.php";
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Recupero email</title>
+        <link href="stile.css" rel="stylesheet" type="text/css">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="container">
+            
+ <form action="upload.php" method="post" enctype="multipart/form-data">
+    Select Image File to Upload:
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
+</form>
+        </div>
+    </body>
+</html>
---
+++ uploaded.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <style>
+    html, body {
+    background: #000;
+    margin: 0;
+    padding: 0;
+    
+    width: 288px;
+    height: 288px;
+}
+
+#video-wrap {
+    background: #000;
+    width: 288px;
+    height: 288px;
+    margin: auto;
+    
+}
+
+#video {
+    display: block;
+    object-fit: cover;
+    width: 288px;
+    height: 288px;
+    margin: auto;
+    font-size: 1px;
+}
+    </style>
+    
+    
+    </head>
+    <body width="288" height="288">
+        
+            
+<?php
+// Include the database configuration file
+include 'connessione.php';
+
+// Get images from the database
+$query = $db->query("SELECT * FROM images ORDER BY uploaded_on DESC");
+
+if($query->num_rows > 0){
+    while($row = $query->fetch_assoc()){
+        $imageURL = 'uploads/'.$row["file_name"];
+?>
+  <img width="288" height="288" src="<?php echo $imageURL; ?>" alt="" />
+<?php }
+}else{ ?>
+    <p>No image(s) found...</p>
+<?php } ?>
+            
+        
+</body>
+
+
